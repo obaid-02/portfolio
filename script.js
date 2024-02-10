2aa51411498115413048be958bff11ba59fa004e
obaid-02 <159303211+obaid-02@users.noreply.github.com>
2024-02-10 12:25:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,24 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
 
-  // Wait for the page to load
+ // Wait for the page to load
 document.addEventListener('DOMContentLoaded', function() {
-    // Alternate between Android Developer and Frontend Developer
+    // Array of developer roles
+    var roles = ['Android Developer', 'Frontend Developer', 'Machine Learning Engineer'];
+    var currentIndex = 0;
+
+    // Function to alternate between developer roles
     setInterval(function() {
         var developerText = document.querySelector('.section__text__p2');
         developerText.classList.remove('show');
+
+        // Increment the index and reset to 0 if it exceeds the length of the array
+        currentIndex = (currentIndex + 1) % roles.length;
+
         setTimeout(function() {
-            if (developerText.textContent === 'Android Developer') {
-                developerText.textContent = 'Frontend Developer';
-            } else {
-                developerText.textContent = 'Android Developer';
-            }
+            developerText.textContent = roles[currentIndex];
             developerText.classList.add('show');
         }, 1000); // Adjust the wait time between transitions (in milliseconds)
     }, 5000); // Adjust the total interval duration (in milliseconds)
 });
+
